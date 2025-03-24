bb2fbb3c8ee6927cfb1c944583bd7a6c66ae0a51
Prince Patel <prince13142004@gmail.com>
2025-03-24 10:52:53 +0530

    change config.py to use secrets.toml

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
+import toml
 
-load_dotenv(override=True)
-
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-if not GROQ_API_KEY:
-    raise ValueError("GROQ_API_KEY not found in environment variables") 
+# Load secrets from the secrets.toml file
+secrets = toml.load('.secrets/secrets.toml')
+GROQ_API_KEY = secrets['GROQ']['API_KEY']
